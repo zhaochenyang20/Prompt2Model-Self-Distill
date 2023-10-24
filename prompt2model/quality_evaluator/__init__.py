b73fe66d0d3f8d0e2f90fe3657ee5214200fe7b0
zhaochen20 <zhaochenyang20@gmail.com>
2023-10-24 16:04:48 +0800

    rename VLLMInformationExtractor

--- prompt2model/quality_evaluator/__init__.py
+++ prompt2model/quality_evaluator/__init__.py
@@ -1,10 +1,10 @@
 """Import quality evaluator classes."""
 from prompt2model.quality_evaluator.base import QualityEvaluator
-from prompt2model.quality_evaluator.information_extractor import InformationExtractor
+from prompt2model.quality_evaluator.information_extractor import VLLMInformationExtractor
 from prompt2model.quality_evaluator.mock import MockQualityEvaluator
 
 __all__ = (
     "QualityEvaluator",
     "MockQualityEvaluator",
-    "InformationExtractor",
+    "VLLMInformationExtractor",
 )
