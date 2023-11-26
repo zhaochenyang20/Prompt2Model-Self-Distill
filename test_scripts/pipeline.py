11a3323be19973fbf9d6a57930b0197051b3b582
cyzhao <zhaochenyang20@gmail.com>
2023-11-26 05:50:00 +0000

    imporve 121

--- test_scripts/pipeline.py
+++ test_scripts/pipeline.py
@@ -2,10 +2,54 @@ import csv
 import json
 import os
 from pathlib import Path
-
 import optuna
+os.environ["CUDA_VISIBLE_DEVICES"] = "6,7"
 from main import main, validate_or_test
 
+
+max_training_epochs = 3
+file_path = "/home/cyzhao/main/NI_tasks/tasks.json"
+with open(file_path, "r", encoding="utf-8") as json_file:
+    all_tasks = json.load(json_file)
+
+tasks = []
+
+# TODO change task name
+task_name = "task121"
+experiment_name = "NI_" + task_name + "_exp_3"
+# TODO change avilable cards
+# Discuss 加入了 metric 需要改写
+for task in all_tasks:
+    if task["task_name"] == task_name:
+        task_tuple = (
+            task["task_name"],
+            task["task_instruction"],
+            task["examples"],
+            task["expected_content"],
+            f"/home/cyzhao/prompt2model_test/testdataset/NI/eval/{task_name}",
+            f"/home/cyzhao/prompt2model_test/testdataset/NI/test/{task_name}",
+            task.get("optional_list", []),
+            task.get("metric", "exact_match"),
+        )
+        tasks.append(task_tuple)
+
+
+log_and_data_root = Path("/home/cyzhao") / experiment_name
+evaluation_result_file_tail = "result.json"
+ckpt_root = Path("/data2/cyzhao/ckpt_data_p2ms")
+best_ckpt_path = Path("/data2/cyzhao/best_ckpt")
+best_validation_result_path = log_and_data_root / "best_validation_result.json"
+log_and_data_root.mkdir(parents=True, exist_ok=True)
+ckpt_root.mkdir(parents=True, exist_ok=True)
+best_ckpt_path.mkdir(parents=True, exist_ok=True)
+# 训练时能够用的显卡，加起来总共剩余的显存对于 7B model 需要接近 200G
+gpu_memory_utilization = 0.80
+tensor_parallel_size = os.environ["CUDA_VISIBLE_DEVICES"].count(",") + 1
+# 进行 inference（除了训练之外的任何步骤）时，会分布在每张卡上，也即 tensor_parallel_size 就是所有能用的 CUDA
+# gpu_memory_utilization 是在每张卡上的占比，比如 CUDA_CONDITION = "0,1,4,5", gpu_memory_utilization = 0.9
+# 则每张卡都会占去全部显存的 0.9，会占用四张卡，推理效率极其高
+# gpu_memory_utilization 越小，则 inference 越慢
+# 然而，不是每张卡都是空的，比如 0 卡已经有人跑了 40G 了，那么 gpu_memory_utilization < 0.5
 
 def write_results(log_and_data_root, max_training_epochs):
     csv_header = [
@@ -50,53 +94,6 @@ def read_json(file_path):
 def print_and_execute_command(command):
     print(command)
     os.system(command)
-
-
-max_training_epochs = 3
-file_path = "/home/cyzhao/main/NI_tasks/tasks.json"
-with open(file_path, "r", encoding="utf-8") as json_file:
-    all_tasks = json.load(json_file)
-
-tasks = []
-
-# TODO change task name
-task_name = "task121"
-experiment_name = "NI_" + task_name + "_exp_3"
-# TODO change avilable cards
-# Discuss 加入了 metric 需要改写
-os.environ["CUDA_VISIBLE_DEVICES"] = "6,7"
-for task in all_tasks:
-    if task["task_name"] == task_name:
-        task_tuple = (
-            task["task_name"],
-            task["task_instruction"],
-            task["examples"],
-            task["expected_content"],
-            f"/home/cyzhao/prompt2model_test/testdataset/NI/eval/{task_name}",
-            f"/home/cyzhao/prompt2model_test/testdataset/NI/test/{task_name}",
-            task.get("optional_list", []),
-            task.get("metric", "exact_match"),
-        )
-        tasks.append(task_tuple)
-
-
-log_and_data_root = Path("/home/cyzhao") / experiment_name
-evaluation_result_file_tail = "result.json"
-ckpt_root = Path("/data2/cyzhao/ckpt_data_p2ms")
-best_ckpt_path = Path("/data2/cyzhao/best_ckpt")
-best_validation_result_path = log_and_data_root / "best_validation_result.json"
-log_and_data_root.mkdir(parents=True, exist_ok=True)
-ckpt_root.mkdir(parents=True, exist_ok=True)
-best_ckpt_path.mkdir(parents=True, exist_ok=True)
-# 训练时能够用的显卡，加起来总共剩余的显存对于 7B model 需要接近 200G
-gpu_memory_utilization = 0.80
-tensor_parallel_size = os.environ["CUDA_VISIBLE_DEVICES"].count(",") + 1
-# 进行 inference（除了训练之外的任何步骤）时，会分布在每张卡上，也即 tensor_parallel_size 就是所有能用的 CUDA
-# gpu_memory_utilization 是在每张卡上的占比，比如 CUDA_CONDITION = "0,1,4,5", gpu_memory_utilization = 0.9
-# 则每张卡都会占去全部显存的 0.9，会占用四张卡，推理效率极其高
-# gpu_memory_utilization 越小，则 inference 越慢
-# 然而，不是每张卡都是空的，比如 0 卡已经有人跑了 40G 了，那么 gpu_memory_utilization < 0.5
-
 
 for task in tasks:
     (
@@ -230,13 +227,13 @@ for task in tasks:
         return highest_validation_result
 
     def objective(trial):
-        generation_epochs = trial.suggest_categorical("generation_epochs", [10, 15, 20])
+        generation_epochs = trial.suggest_categorical("generation_epochs", [10, 20, 30])
         generation_batch_size = trial.suggest_categorical(
             "generation_batch_size", [10, 15, 20]
         )
         generation_top_k = trial.suggest_categorical("generation_top_k", [40, 45, 50])
         generation_temperature = trial.suggest_categorical(
-            "generation_temperature", [0.75, 0.80, 0.85]
+            "generation_temperature", [0.85, 0.90, 0.95]
         )
         min_frequency = trial.suggest_categorical(
             "min_frequency", [0.3, 0.35, 0.4]
