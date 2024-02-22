954f187cf750cf9371bbd51251cc306bb49ab394
azure <zhaochenyang20@gmail.com>
2024-02-22 14:44:43 +0000

    some update for sft_pipeline

--- test_scripts/unit_test/sft_pipeline.py
+++ test_scripts/unit_test/sft_pipeline.py
@@ -17,14 +17,14 @@ from prompt2model.utils.path import ROOT, STORE_ROOT, MODEL_PATH
 model_path = Path(
     MODEL_PATH
 )
-ckpt_path = Path(STORE_ROOT+"/ckpt")
-generated_dataset_path = Path(ROOT+"/generated_datasets")
+# ckpt_path = Path(STORE_ROOT+"/ckpt")
+# generated_dataset_path = Path(ROOT+"/generated_datasets")
 dataset_path = Path(
     ROOT+"/generation_squad/squad_exp_1/squad_0.6_False_False_1/dataset"
 )
 
 prompt_spec = MockPromptSpec(
-    task_type=TaskType.TEXT_GENERATION,
+    task_type=TaskType.CLASSIFICATION,
     instruction="Your task is to generate an answer to a natural question. In this task, the input is a string that consists of both a question and a context passage. The context is a descriptive passage related to the question and contains the answer. And the question can range from Math, Cultural, Social, Geometry, Biology, History, Sports, Technology, Science, and so on.",  # # noqa E501
     examples="""
 [input]="Question: What city did Super Bowl 50 take place in? Context: Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season. The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California. As this was the 50th Super Bowl, the league emphasized the "golden anniversary" with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as "Super Bowl L"), so that the logo could prominently feature the Arabic numerals 50."
