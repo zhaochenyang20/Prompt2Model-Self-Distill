beb066c826a9016d3fb3b696ba66b600d4361ee3
Xueying <xjia2@andrew.cmu.edu>
2024-03-22 00:15:54 +0000

    Update exact match metric

--- test_scripts/utils/evaluate.py
+++ test_scripts/utils/evaluate.py
@@ -1,15 +1,46 @@
+import string
+import re
+from collections import Counter
+
+def find_last_occurrence(model_output: str, labels: list[str]) -> str:
+    pattern = '|'.join(re.escape(label) for label in labels)
+    regex = re.compile(pattern)
+    matches = list(regex.finditer(model_output))
+    return matches[-1].group() if matches else None
+
+# cited from https://github.com/allenai/natural-instructions/blob/55a365637381ce7f3748fa2eac7aef1a113bbb82/eval/automatic/evaluation.py#L24
+def normalize_answer(s):
+    """Lower text and remove punctuation, and extra whitespace."""
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_punc(lower(s)))
+
+def exact_match(prediction, ground_truth, xlingual=False):
+    # small changed based on our current code
+    if prediction is None:
+        return 0
+    return (normalize_answer(prediction) == normalize_answer(ground_truth))
+
 def exact_match_score(
     GROUND_TRUTH,
     tuned_model_generated_outputs,
 ):
+    labels = list(Counter(GROUND_TRUTH).keys())
     index = 0
-    for i in range(len(GROUND_TRUTH)):
-        if (GROUND_TRUTH[i] in tuned_model_generated_outputs[i]) or (tuned_model_generated_outputs[i] in GROUND_TRUTH[i]):
-            if tuned_model_generated_outputs[i] != "":
-                index += 1
+    n = len(GROUND_TRUTH)
+    for i in range(n):
+        index += exact_match(find_last_occurrence(tuned_model_generated_outputs[i], labels), GROUND_TRUTH[i])
     exact_match = index / len(GROUND_TRUTH)
     return exact_match
-
 
 def lcs_length_dp(x, y):
     """Compute the length of the longest common subsequence between two strings using dynamic programming."""
