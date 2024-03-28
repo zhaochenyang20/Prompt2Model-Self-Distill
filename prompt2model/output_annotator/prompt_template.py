8ba9cc0b3cc596749dba0a98dc3db4eea8a5d81f
Xueying <xjia2@andrew.cmu.edu>
2024-03-28 22:10:10 +0000

    add label reversal option for constructing prompt

--- prompt2model/output_annotator/prompt_template.py
+++ prompt2model/output_annotator/prompt_template.py
@@ -1,6 +1,7 @@
 """Utilities to construct an LLM "metaprompt" for our dataset generator."""
 
 import re
+import random
 
 GENERATION_PROMPT_TEMPLATE = """
 ### Instructions:
@@ -29,6 +30,12 @@ USER: [input] {notion} {new_input}
 ASSISTANT:
 """  # noqa E501
 
+def reverse_func(labels):
+    number_labels = len(labels)
+    new_indexes = list(range(number_labels))
+    new_index = random.choice(new_indexes)
+    return labels[new_index]
+
 def construct_meta_prompt(
     instruction: str = None,
     examples: str = None,
@@ -36,7 +43,9 @@ def construct_meta_prompt(
     is_generation: bool = True,
     few_shots_prompt: str = None,
     notion: str = '=',
-    suffix: str = '\n'
+    suffix: str = '\n',
+    reverse: bool = False,
+    labels: list = []
 ) -> str:
     """Constructs a prompt template for the dataset generator.
 
@@ -51,29 +60,42 @@ def construct_meta_prompt(
         re.DOTALL,
     )
     assert matches != []
-    annotation_prompt_string = ""
-    if is_generation:
-        for input, output in matches:
-            annotation_prompt_string += f"[input] {notion} {input}{suffix}"
-            annotation_prompt_string += f"[output] {notion} {output}{suffix}"
-        assert annotation_prompt_string != ""
-        prompt = GENERATION_PROMPT_TEMPLATE.format(
-            instruction=instruction,
-            new_input=new_input,
-            examples=annotation_prompt_string.strip(),
-            few_shots_prompt=few_shots_prompt
-        )
+
+    # reverse labels in matches (few-shots examples)
+    fixed_few_shots_examples = []
+    if reverse:
+        for match in matches:
+            random_label = reverse_func(labels=labels)
+            example = (match[0], random_label)
+            fixed_few_shots_examples.append(example)
     else:
-        annotation_prompt_string = ""
-        for input, output in matches:
-            annotation_prompt_string += f"USER: [input] {notion} {input}{suffix}"
-            annotation_prompt_string += f"ASSISTANT: {output}{suffix}"
-        assert annotation_prompt_string != ""
-        prompt = CLASSIFICATION_PROMPT_TEMPLATE.format(
-            instruction=instruction,
-            new_input=new_input,
-            examples=annotation_prompt_string.strip(),
-            few_shots_prompt=few_shots_prompt,
-            notion=notion
-        )
+        fixed_few_shots_examples = matches
+
+
+    annotation_prompt_string = ""
+    # is_generation is always false
+    # if is_generation:
+    #     for input, output in matches:
+    #         annotation_prompt_string += f"[input] {notion} {input}{suffix}"
+    #         annotation_prompt_string += f"[output] {notion} {output}{suffix}"
+    #     assert annotation_prompt_string != ""
+    #     prompt = GENERATION_PROMPT_TEMPLATE.format(
+    #         instruction=instruction,
+    #         new_input=new_input,
+    #         examples=annotation_prompt_string.strip(),
+    #         few_shots_prompt=few_shots_prompt
+    #     )
+    # else:
+    annotation_prompt_string = ""
+    for input, output in fixed_few_shots_examples:
+        annotation_prompt_string += f"USER: [input] {notion} {input}{suffix}"
+        annotation_prompt_string += f"ASSISTANT: {output}{suffix}"
+    assert annotation_prompt_string != ""
+    prompt = CLASSIFICATION_PROMPT_TEMPLATE.format(
+        instruction=instruction,
+        new_input=new_input,
+        examples=annotation_prompt_string.strip(),
+        few_shots_prompt=few_shots_prompt,
+        notion=notion
+    )
     return prompt
